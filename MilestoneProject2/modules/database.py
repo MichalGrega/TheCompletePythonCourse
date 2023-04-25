719446293dfeaba51557441240ff150959bda24d
MichalGrega <101899497+MichalGrega@users.noreply.github.com>
2023-04-25 13:13:21 +0200

    context manager introduced

--- MilestoneProject2/modules/database.py
+++ MilestoneProject2/modules/database.py
@@ -1,51 +1,38 @@
-import sqlite3
+from .database_connection import DatabaseConnection
 
 def add(title, author):
-    create_table()
-    db = sqlite3.connect("books.db")
-    cursor = db.cursor()
-    try:
-        cursor.execute(f"INSERT INTO books(title, author, read) values (?, ?, 0)", (title, author))
-    except:
-        cursor.execute(f"UPDATE books SET author = ?, read = 0 WHERE title = ?", (author, title))
-    db.commit()
-    db.close()
+    with DatabaseConnection("books.db") as db:
+        cursor = db.cursor()
+        cursor = db.cursor()
+        try:
+            cursor.execute(f"INSERT INTO books(title, author, read) values (?, ?, 0)", (title, author))
+        except:
+            cursor.execute(f"UPDATE books SET author = ?, read = 0 WHERE title = ?", (author, title))
 
 def create_table():
-    db = sqlite3.connect("books.db")
-    cursor = db.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS books(title text PRIMARY KEY, author text, read integer)")
-    db.commit()
-    db.close()
+    with DatabaseConnection("books.db") as db:
+        cursor = db.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS books(title text PRIMARY KEY, author text, read integer)")
 
 def get():
     create_table()
-    db = sqlite3.connect("books.db")
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM books")
-    return [{'title': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()]
+    with DatabaseConnection("books.db") as db:
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM books")
+        return [{'title': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()]
     
 
 def mark_read(book_title: str):
-    db = sqlite3.connect("books.db")
-    cursor = db.cursor()
-    cursor.execute(f"UPDATE books SET read = 1 WHERE title = '{book_title}'")
-    db.commit()
-    db.close()
+    with DatabaseConnection("books.db") as db:
+        cursor = db.cursor()
+        cursor.execute(f"UPDATE books SET read = 1 WHERE title = '{book_title}'")
 
 def drop():
-    db = sqlite3.connect("books.db")
-    cursor = db.cursor()
-    cursor.execute("DROP TABLE books")
-    db.commit()
-    db.close()
+    with DatabaseConnection("books.db") as db:
+        cursor = db.cursor()
+        cursor.execute("DROP TABLE books")
 
 def rm(title: str):
-    db = sqlite3.connect("books.db")
-    cursor = db.cursor()
-    print(title)
-    # cursor.execute(f"DELETE FROM books WHERE title = '{title}'")
-    print((title))
-    cursor.execute(f"DELETE FROM books WHERE title = ?", (title,))
-    db.commit()
-    db.close()
+    with DatabaseConnection("books.db") as db:
+        cursor = db.cursor()
+        cursor.execute(f"DELETE FROM books WHERE title = ?", (title,))
